4961d879af84f3a50706d65ccd00b40df9314f8c
Quintin Herb <33743358+quintanamo@users.noreply.github.com>
2019-05-13 23:46:54 -0400

    Add files via upload

--- EntreLink/src/edu/ycp/cs320/entrelink/servlet/IndexServlet.java
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/IndexServlet.java
@@ -9,13 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import edu.ycp.cs320.entrelink.controller.PostController;
+import edu.ycp.cs320.entrelink.controller.AllPostsController;
 import edu.ycp.cs320.entrelink.model.Post;
 
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	private PostController controller = null;
+	private AllPostsController controller = null;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -32,7 +32,7 @@ public class IndexServlet extends HttpServlet {
 		ArrayList<Post> bPosts = null;
 		String errorMessage = null;
 
-		controller = new PostController();
+		controller = new AllPostsController();
 
 		// get list of authors returned from query
 		posts = controller.getAllPosts("student");
---
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/LogoutServlet.java
@@ -0,0 +1,32 @@
+package edu.ycp.cs320.entrelink.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.ycp.cs320.entrelink.controller.AllPostsController;
+import edu.ycp.cs320.entrelink.model.Post;
+
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private AllPostsController controller = null;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		session.setAttribute("loggedInName", null);
+		Boolean sessionExists = session.isNew();
+        
+		
+		System.out.println("Logout Servlet: doGet");
+
+		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+	}
+}
