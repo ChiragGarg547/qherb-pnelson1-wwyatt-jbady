a3b56be248a9f12907f018deeece309b6538e77b
Patrick Nelson <48131480+pdnelson@users.noreply.github.com>
2019-05-14 19:19:10 -0400

    Update LogoutServlet.java

--- EntreLink/src/edu/ycp/cs320/entrelink/servlet/LogoutServlet.java
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/LogoutServlet.java
@@ -9,14 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import edu.ycp.cs320.entrelink.controller.AllPostsController;
 import edu.ycp.cs320.entrelink.model.Post;
 
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	private AllPostsController controller = null;
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
