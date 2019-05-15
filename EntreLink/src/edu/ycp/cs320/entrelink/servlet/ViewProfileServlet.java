fd712788e44ff8241e3c4e73f026284e65642904
Quintin Herb <33743358+quintanamo@users.noreply.github.com>
2019-05-14 20:45:58 -0400

    Add files via upload

---
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/ViewProfileServlet.java
@@ -0,0 +1,44 @@
+package edu.ycp.cs320.entrelink.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.ycp.cs320.entrelink.model.Post;
+
+public class ViewProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		session.setAttribute("loggedInName", null);
+		Boolean sessionExists = session.isNew();
+        
+		
+		String parseURL = req.getPathInfo();
+		parseURL = parseURL.substring(1, parseURL.length());
+		int userID = 0;
+		
+		try {
+			userID = Integer.parseInt(parseURL);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		session.setAttribute("userID", parseURL);
+		
+		System.out.println("View Profile Servlet: doGet");
+		System.out.println("User ID: " + parseURL);
+
+		req.getRequestDispatcher("/_view/viewProfile.jsp").forward(req, resp);
+	
+	}
+}
